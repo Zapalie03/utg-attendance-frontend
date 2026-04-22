0dd97a47db9f70d8ced83a99356d2c580e843361
Abdoulie Bah <bahablie03@gmail.com>
2026-04-22 10:58:38 +0000

    Update API URL to production backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://utg-attendance-backend.onrender.com/api'
 });
-
 // Automatically attach token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
